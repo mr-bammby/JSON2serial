7a681d76346cf1edd8c6285f13dd9764ceaeee2b
mr-bammby <54394409+mr-bammby@users.noreply.github.com>
2022-05-20 10:31:27 +0200

    Added loop

--- main.py
+++ main.py
@@ -19,17 +19,22 @@ if 'log_file' in setup.keys():
 else:
     USBconn = usb_driver.USB_conn(setup['com'], setup['baudrate'], setup['timeout'])
 
-if setup['type'] == "relative":
-    for line in data['timeline']:
-        time.sleep(line['delay_ms'] / 1000.0)
-        toUSB(line)
-if setup['type'] == "absolute":
-    start_time_ns = time.time_ns()
-    for line in data['timeline']:
-        while True:
-            time_ns = time.time_ns()
-            if ((time_ns - start_time_ns) / 1000000.0) > line['delay_ms']:
-                break
-            time.sleep(5/10000)
-        toUSB(line)
+USBconn.write(0, 1)
+USBconn.write(0, 2)
+while (1):
+    if setup['type'] == "relative":
+        for line in data['timeline']:
+            time.sleep(line['delay_ms'] / 1000.0)
+            toUSB(line)
+    if setup['type'] == "absolute":
+        start_time_ns = time.time_ns()
+        for line in data['timeline']:
+            while True:
+                time_ns = time.time_ns()
+                if ((time_ns - start_time_ns) / 1000000.0) > line['delay_ms']:
+                    break
+                time.sleep(5/10000)
+            toUSB(line)
+    if setup['loop'] == "single":
+        break
 USBconn.close()
